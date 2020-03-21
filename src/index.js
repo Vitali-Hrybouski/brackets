ee405c909cf2170236304bb46873d9b527e7728a
Vitali-Hrybouski <60929162+Vitali-Hrybouski@users.noreply.github.com>
2020-03-21 14:34:10 +0300

    feat: add solution task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  function delBrackets(str, bracketsConfig) {
+    const testString = str;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      const element = bracketsConfig[i].join("");
+      while (~str.indexOf(element)) str = str.replace(element, "");
+    }
+    if (str !== testString) return delBrackets(str, bracketsConfig);
+    else return str;
+  }
+  if (delBrackets(str, bracketsConfig) == 0) return true;
+  else return false;
 }
